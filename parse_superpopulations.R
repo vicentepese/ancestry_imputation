1e96342580e217c0991be601cc2758a7f095b484
vicentepese <sempereperisvicente@gmail.com>
2021-01-14 11:00:28 -0800

    Parse superpopulations 1000 Genome

--- parse_superpopulations.R
+++ parse_superpopulations.R
@@ -33,3 +33,9 @@ for (i in 1:nrow(sample.info)){
     
   }
 }
+
+# Create dataframe 
+sample.ethnicity <- data.frame("Sample" = sample.info$Sample, "Population" = superpop) 
+
+# Wwrite 
+write.table(sample.ethnicity, file = settings$Resources$thougen_ethnicity, quote = FALSE, sep = ',', row.names = FALSE, col.names = TRUE)
